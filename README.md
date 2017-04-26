321cc0cb42a96897a4a4be30e9c1e95f54b476c1
Jeffrey Zeng <jezeng@microsoft.com>
2017-04-26 12:11:46 -0700

    Combine http plugin for better throughput

--- README.md
+++ README.md
@@ -1,98 +1,70 @@
-# Logstash Plugin
+# Logstash output plugin for sending data to Microsoft Operations Management Suite (OMS)
 
-[![Travis Build Status](https://travis-ci.org/logstash-plugins/logstash-output-example.svg)](https://travis-ci.org/logstash-plugins/logstash-output-example)
+## Summary
+This plugin sends Logstash events to the specified Microsoft OMS workspace.
 
-This is a plugin for [Logstash](https://github.com/elastic/logstash).
+## Installation
 
-It is fully free and fully open source. The license is Apache 2.0, meaning you are pretty much free to use it however you want in whatever way.
-
-## Documentation
-
-Logstash provides infrastructure to automatically generate documentation for this plugin. We use the asciidoc format to write documentation so any comments in the source code will be first converted into asciidoc and then into html. All plugin documentation are placed under one [central location](http://www.elastic.co/guide/en/logstash/current/).
-
-- For formatting code or config example, you can use the asciidoc `[source,ruby]` directive
-- For more asciidoc formatting tips, see the excellent reference here https://github.com/elastic/docs#asciidoc-guide
-
-## Need Help?
-
-Need help? Try #logstash on freenode IRC or the https://discuss.elastic.co/c/logstash discussion forum.
-
-## Developing
-
-### 1. Plugin Developement and Testing
-
-#### Code
-- To get started, you'll need JRuby with the Bundler gem installed.
-
-- Create a new plugin or clone and existing from the GitHub [logstash-plugins](https://github.com/logstash-plugins) organization. We also provide [example plugins](https://github.com/logstash-plugins?query=example).
-
-- Install dependencies
+First, build the plugin gem:
 ```sh
-bundle install
+gem build logstash-output-oms.gemspec
 ```
 
-#### Test
-
-- Update your dependencies
-
+You can then install this plugin using the Logstash "plugin" or "logstash-plugin" (for newer versions of Logstash) command:
 ```sh
-bundle install
+logstash-plugin install logstash-output-oms-[VERSION].gem
 ```
 
-- Run tests
+For more information, see Logstash reference [Working with plugins](https://www.elastic.co/guide/en/logstash/current/working-with-plugins.html).
 
+## Testing
+
+First, replace the following variables in spec/outputs/oms_spec.rb with values specific to your OMS workspace:
+- let(:workspace_id) { "---- WORKSPACE ID ----" }
+- let(:shared_key) { "---- SHARED KEY ----" }
+- let(:log_type) { "---- LOG TYPE ----" }
+
+Run the following command:
 ```sh
 bundle exec rspec
 ```
 
-### 2. Running your unpublished Plugin in Logstash
+## Configuration
+### Required Parameters
+__*shared_key*__
 
-#### 2.1 Run in a local Logstash clone
+The shared access key to the target workspace.
 
-- Edit Logstash `Gemfile` and add the local plugin path, for example:
-```ruby
-gem "logstash-filter-awesome", :path => "/your/local/logstash-filter-awesome"
+OR
+
+__*oms_creds_file*__
+
+A file containing the shared access key to the target workspace.
+
+AND
+
+__*workspace_id*__
+
+The OMS workspace ID.
+
+AND
+
+__*log_type*__
+
+The type of logs being sent to OMS.
+
+### Examples
 ```
-- Install plugin
-```sh
-# Logstash 2.3 and higher
-bin/logstash-plugin install --no-verify
-
-# Prior to Logstash 2.3
-bin/plugin install --no-verify
-
+output
+{
+    oms
+    {
+        shared_key => "XXXXXXXXXXXXXXXXXXX"
+        workspace_id => "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+        log_type => "syslog"
+    }
+}
 ```
-- Run Logstash with your plugin
-```sh
-bin/logstash -e 'filter {awesome {}}'
-```
-At this point any modifications to the plugin code will be applied to this local Logstash setup. After modifying the plugin, simply rerun Logstash.
 
-#### 2.2 Run in an installed Logstash
-
-You can use the same **2.1** method to run your plugin in an installed Logstash by editing its `Gemfile` and pointing the `:path` to your local plugin development directory or you can build the gem and install it using:
-
-- Build your plugin gem
-```sh
-gem build logstash-filter-awesome.gemspec
-```
-- Install the plugin from the Logstash home
-```sh
-# Logstash 2.3 and higher
-bin/logstash-plugin install --no-verify
-
-# Prior to Logstash 2.3
-bin/plugin install --no-verify
-
-```
-- Start Logstash and proceed to test the plugin
-
-## Contributing
-
-All contributions are welcome: ideas, patches, documentation, bug reports, complaints, and even something you drew up on a napkin.
-
-Programming is not a required skill. Whatever you've seen about open source and maintainers or community members  saying "send patches or die" - you will not see that here.
-
-It is more important to the community that you are able to contribute.
-
-For more information about contributing, see the [CONTRIBUTING](https://github.com/elastic/logstash/blob/master/CONTRIBUTING.md) file.
+## More information
+We welcome you to provide feedback and/or contribute to the project.
--- lib/logstash/outputs/oms.rb
+++ lib/logstash/outputs/oms.rb
@@ -1,46 +1,209 @@
 # encoding: utf-8
 require "logstash/outputs/base"
 require "logstash/namespace"
-require "base64"
-require "digest"
-require 'json'
-require 'net/http'
-require 'net/https'
-require 'openssl'
-require 'socket'
-require 'time'
-require 'uri'
+require "logstash/json"
+require "uri"
+require "logstash/plugin_mixins/http_client"
 
-# An example output that does nothing.
 class LogStash::Outputs::OMS < LogStash::Outputs::Base
+  include LogStash::PluginMixins::HttpClient
+
+  # This output lets you send events to an OMS workspace via HTTP(S)
+  #
+  # This output will execute up to 'pool_max' requests in parallel for performance.
+  # Consider this when tuning this plugin for performance.
+  #
+  # Additionally, note that when parallel execution is used strict ordering of events is not
+  # guaranteed!
+  #
+  # Beware, this gem does not yet support codecs. Please use the 'format' option for now.
+
   config_name "oms"
 
+  # Content type
+  #
+  # If not specified, this defaults to the following:
+  #
+  # * if format is "json", "application/json"
+  # * if format is "form", "application/x-www-form-urlencoded"
+  config :content_type, :validate => :string
+
+  # This lets you choose the structure and parts of the event that are sent.
+  #
+  #
+  # For example:
+  # [source,ruby]
+  #    mapping => {"foo", "%{host}", "bar", "%{type}"}
+  config :mapping, :validate => :hash
+
+  # Set the format of the http body.
+  #
+  # If form, then the body will be the mapping (or whole event) converted
+  # into a query parameter string, e.g. `foo=bar&baz=fizz...`
+  #
+  # If message, then the body will be the result of formatting the event according to message
+  #
+  # Otherwise, the event is sent as json.
+  config :format, :validate => ["json", "form", "message"], :default => "json"
+
+  config :message, :validate => :string
+
   # The OMS Customer ID to use
-  config :workspaceID, :validate => :string, :required => :true
+  config :workspace_id, :validate => :string, :required => :true
 
   # The OMS Shared Key to use
-  config :sharedKey, :validate => :string
+  config :shared_key, :validate => :string
 
   # OMS credentials file
   config :oms_creds_file, :validate => :string
 
   # The OMS Log Type to use
-  config :logType, :required => :true
+  config :log_type, :required => :true
 
-  public
   def register
-    if (@oms_creds_file and @sharedKey)
-      raise(LogStash::ConfigurationError, "Only one of :sharedKey, :oms_creds_file may be set at a time.")
+    @http_method = "post".to_sym
+
+    # We count outstanding requests with this queue
+    # This queue tracks the requests to create backpressure
+    # When this queue is empty no new requests may be sent,
+    # tokens must be added back by the client on success
+    @request_tokens = SizedQueue.new(@pool_max)
+    @pool_max.times {|t| @request_tokens << true }
+
+    @requests = Array.new
+
+    if @content_type.nil?
+      case @format
+        when "form" ; @content_type = "application/x-www-form-urlencoded"
+        when "json" ; @content_type = "application/json"
+        when "message" ; @content_type = "text/plain"
+      end
     end
 
-    @sharedKey = File.read(@oms_creds_file).strip if @oms_creds_file
+    if (@oms_creds_file and @shared_key)
+      raise(LogStash::ConfigurationError, "Only one of :shared_key, :oms_creds_file may be set at a time.")
+    end
+
+    @shared_key = File.read(@oms_creds_file).strip if @oms_creds_file
+
+    validate_format!
+  end # def register
+
+  def multi_receive(events)
+    events.each {|event| receive(event, :parallel)}
+    client.execute!
   end
 
-  public
+  # Once we no longer need to support Logstash < 2.2 (pre-ng-pipeline)
+  # We don't need to handle :background style requests
+  #
+  # We use :background style requests for Logstash < 2.2 because before the microbatching
+  # pipeline performance is greatly improved by having some degree of async behavior.
+  #
+  # In Logstash 2.2 and after things are much simpler, we just run each batch in parallel
+  # This will make performance much easier to reason about, and more importantly let us guarantee
+  # that if `multi_receive` returns all items have been sent.
+  def receive(event, async_type=:background)
+    body = event_body(event)
 
-  def receive(event)
-    post_data(event)
-    return "Event received"
+    # Block waiting for a token
+    token = @request_tokens.pop if async_type == :background
+
+    # Send the request
+    url = "https://" + workspace_id + ".ods.opinsights.azure.com/api/logs?api-version=2016-04-01" 
+    headers = generate_headers(event)
+
+    # Create an async request
+    request = client.send(async_type).send(@http_method, url, :body => body, :headers => headers)
+
+    request.on_complete do
+      # Make sure we return the token to the pool
+      @request_tokens << token  if async_type == :background
+    end
+
+    request.on_success do |response|
+      if response.code < 200 || response.code > 299
+        log_failure(
+          "Encountered non-200 HTTP code #{200}",
+          :response_code => response.code,
+          :url => url,
+          :event => event)
+      end
+    end
+
+    request.on_failure do |exception|
+      log_failure("Could not fetch URL",
+                  :url => url,
+                  :method => @http_method,
+                  :body => body,
+                  :headers => headers,
+                  :message => exception.message,
+                  :class => exception.class.name,
+                  :backtrace => exception.backtrace
+      )
+    end
+
+    request.call if async_type == :background
+  end
+
+  def close
+    client.close
+  end
+
+  private
+
+  # This is split into a separate method mostly to help testing
+  def log_failure(message, opts)
+    @logger.error("[OMS Output Failure] #{message}", opts)
+  end
+
+  # Manticore doesn't provide a way to attach handlers to background or async requests well
+  # It wants you to use futures. The #async method kinda works but expects single thread batches
+  # and background only returns futures.
+  # Proposed fix to manticore here: https://github.com/cheald/manticore/issues/32
+  def request_async_background(request)
+    @method ||= client.executor.java_method(:submit, [java.util.concurrent.Callable.java_class])
+    @method.call(request)
+  end
+
+  # Format the HTTP body
+  def event_body(event)
+    # TODO: Create an HTTP post data codec, use that here
+    if @format == "json"
+      LogStash::Json.dump(map_event(event))
+    elsif @format == "message"
+      event.sprintf(@message)
+    else
+      encode(map_event(event))
+    end
+  end
+
+  def map_event(event)
+    if @mapping
+      @mapping.reduce({}) do |acc,kv|
+        k,v = kv
+        acc[k] = event.sprintf(v)
+        acc
+      end
+    else
+      event.to_hash
+    end
+  end
+
+  def generate_headers(event)
+    msg = LogStash::Json.dump(event)
+
+    date = Time.now.utc.httpdate()
+    contentLength = msg.bytesize
+    signature = build_signature(date, contentLength)
+
+    headers = {}
+
+    headers[CaseSensitiveString.new("Authorization")] = signature
+    headers[CaseSensitiveString.new("Log-Type")] = log_type
+    headers[CaseSensitiveString.new("x-ms-date")] = date
+    headers["Content-Type"] = 'application/json'
+    return headers
   end
 
   def build_signature(date, contentLength)
@@ -51,84 +214,36 @@ class LogStash::Outputs::OMS < LogStash::Outputs::Base
     xHeaders = "x-ms-date:#{date}"
     stringToHash = "#{method}\n#{contentLength}\n#{contentType}\n#{xHeaders}\n#{resource}"
 
-    key = Base64.decode64(sharedKey)
+    key = Base64.decode64(shared_key)
     hash = Base64.encode64(OpenSSL::HMAC.digest('sha256', key, stringToHash)).strip
-    signature = "SharedKey #{workspaceID}:#{hash}"
+    signature = "SharedKey #{workspace_id}:#{hash}"
 
     return signature
   end # build_signature
 
-  def post_data(records)
-    msg = JSON.dump(records)
+  #TODO Extract this to a codec
+  def encode(hash)
+    return hash.collect do |key, value|
+      CGI.escape(key) + "=" + CGI.escape(value.to_s)
+    end.join("&")
+  end
 
-    date = Time.now.utc.httpdate()
-    contentLength = msg.bytesize
-    signature = build_signature(date, contentLength)
 
-    headers = {}
-
-    headers[CaseSensitiveString.new("Authorization")] = signature
-    headers[CaseSensitiveString.new("Log-Type")] = logType
-    headers[CaseSensitiveString.new("x-ms-date")] = date
-    headers["Content-Type"] = 'application/json'
-    headers["Content-Length"] = contentLength.to_s
-
-    req = Net::HTTP::Post.new("https://" + workspaceID + ".ods.opinsights.azure.com/api/logs?api-version=2016-04-01", headers)
-    req.body = msg
-
-    http = create_secure_http( URI.parse("https://" + workspaceID + ".ods.opinsights.azure.com/api/logs?api-version=2016-04-01") , "")
-    start_request(req, http)
-
-    return contentLength
-  end # post_data
-
-  def start_request(req, secure_http, ignore404 = false)
-      # Tries to send the passed in request
-      # Raises an exception if the request fails.
-      # This exception should only be caught by the fluentd engine so that it retries sending this
-    begin
-        res = nil
-        res = secure_http.start { |http|  http.request(req) }
-	  rescue => e
-  		@logger.error("Error in section 1  #{e.message}")
-    else
-
-      if res.nil?
-  		    @logger.error("Failed to request method")
-  	  end
-
-  	  if res.is_a?(Net::HTTPSuccess)
-        return res.body
+  def validate_format!
+    if @format == "message"
+      if @message.nil?
+        raise "message must be set if message format is used"
       end
 
-      if ignore404 and res.code == "404"
-        return ''
+      if @content_type.nil?
+        raise "content_type must be set if message format is used"
       end
 
-      if res.code != "200"
-        # Retry all failure error codes...
-        res_summary = "(class=#{res.class.name}; code=#{res.code}; message=#{res.message}; body=#{res.body};)"
-        @logger.error("Failed #{res_summary}")
+      unless @mapping.nil?
+        @logger.warn "mapping is not supported and will be ignored if message format is used"
       end
-
-    end # end begin
-  end # end start_request
-
-  # create an HTTP object which uses HTTPS
-  def create_secure_http(uri, proxy={})
-    if proxy.empty?
-      http = Net::HTTP.new( uri.host, uri.port )
-    else
-      http = Net::HTTP.new( uri.host, uri.port,
-                            proxy[:addr], proxy[:port], proxy[:user], proxy[:pass])
     end
-
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    http.open_timeout = 30
-    return http
-  end # create_secure_http
-
+  end
 end # class LogStash::Outputs::OMS
 
 class CaseSensitiveString < String
